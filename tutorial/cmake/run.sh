6a761288143aad12fb11c1b46b3df85e6e024add
KangYong <kangyong0815@gmail.com>
2023-07-27 05:10:49 -0400

    Update run.sh

--- tutorial/cmake/run.sh
+++ tutorial/cmake/run.sh
@@ -1,3 +1,6 @@
 video_file = /disk/videos/road.mov
 
 build/main -v $video_file
+
+
+g++ asift.cpp -I/usr/include/opencv4/ -lopencv_videoio -lopencv_highgui -lopencv_imgproc -lopencv_core -lopencv_imgcodecs -lopencv_photo -lopencv_imgproc -lopencv_calib3d -lopencv_features2d
